1c0e3f23f0863031e31915a375c182d2e18404ab
Alexios Karadimos <karadalex@gmail.com>
2023-04-23 15:57:17 +0300

    [hg] slight improvement - getting to run the simulation

--- mujoco-atlas-simulation/atlas_sim.py
+++ mujoco-atlas-simulation/atlas_sim.py
@@ -1,12 +1,14 @@
 import mujoco_py
+import numpy as np
 
 model = mujoco_py.load_model_from_path('atlas.xml')
 sim = mujoco_py.MjSim(model)
 view = mujoco_py.MjViewer(sim)
 
 # Set initial pose to crouched position
-crouched_pos = [0.0, 0.0, 0.9, -1.6, 0.8, 0.0, 0.0, 0.0, 0.0, -0.9, 1.6, -0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-sim.data.qpos[:] = crouched_pos
+print(sim.data.qpos)
+crouched_pos = np.array([0.0, 0.0, 0.9, -1.6, 0.8, 0.0, 0.0, 0.0, 0.0, -0.9, 1.6, -0.8])
+sim.data.qpos[:12] = crouched_pos
 sim.forward()
 
 # Simulation parameters
@@ -16,18 +18,23 @@ ctrl_delay = 10
 ctrl_time = 1.0 / ctrl_freq
 
 # PD controller gains
-kp = 100
-kd = 1
+kp = 0.5
+kd = 0.1
+
+# Desired standing position
+# standing_pos = np.array([0.0, 0.0, 1.25, -1.5, 0.0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 0.0])
+standing_pos = np.array([0.0, -1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
 # Main loop
+i = 0
 while True:
     # Compute joint torques using PD controller
-    qpos = sim.data.qpos
-    qvel = sim.data.qvel
+    qpos = sim.data.qpos[:12]
+    qvel = sim.data.qvel[:12]
     tau = -kp * (qpos - crouched_pos) - kd * qvel
 
     # Apply torques to robot joints
-    sim.data.ctrl[:] = tau
+    sim.data.ctrl[:12] = tau
 
     # Step simulation
     sim.step()
@@ -35,10 +42,12 @@ while True:
 
     # Delay control loop
     if i % ctrl_delay == 0:
-        mujoco_py.functions.mj_step1(model.ptr, sim.data.ptr)
+        mujoco_py.functions.mj_step1(model, sim.data)
 
     # Print robot state
     if i % ctrl_freq == 0:
         print('Step:', i)
         print('Position:', sim.data.qpos[:7])
         print('Velocity:', sim.data.qvel[:6])
+
+    i+=1
